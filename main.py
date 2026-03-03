6302b53e6e43e8d72fd7c4078151db345cc36310
Angelo200802 <angelo.capogrosso02@gmail.com>
2026-03-03 09:32:04 +0100

    Refactor load_csv_data function signature and enhance data handling

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ DATASET_COLUMN_NAMES = {
     "Y2": "Cooling Load"
 }
 
-def load_csv_data(file_path):
+def load_csv_data(file_path: str) -> pd.DataFrame:
     """Load data from a CSV file."""
     try:
         data = pd.read_csv(file_path)
@@ -28,6 +28,7 @@ def load_csv_data(file_path):
     
 if __name__ == "__main__":
     file_path = os.getenv("DATASET_PATH")
-    data = load_csv_data(file_path)
+    data: pd.DataFrame = load_csv_data(file_path)
+    data.rename(columns=DATASET_COLUMN_NAMES, inplace=True) 
     if data is not None:
-        print(data.head())  # Print the first few rows of the data
+        print(data.head()) 
